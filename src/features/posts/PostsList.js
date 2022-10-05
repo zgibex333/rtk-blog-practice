79862e5ef4b854dfe7bdc96d1a2a86a13aec8780
Den CZ <zgibex333@gmail.com>
2022-10-05 17:51:29 +0300

    02_sync_rtk

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+import PostAuthor from "./PostAuthor";
+import { selectAllPosts } from "./postsSlice";
+import ReactionButtons from "./ReactionButtons";
+import TimeAgo from "./TimeAgo";
+
+const PostsList = () => {
+  const posts = useSelector(selectAllPosts);
+
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <p>{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+    </article>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
+
+export default PostsList;
